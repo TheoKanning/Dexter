2819c50a38f44a36d56c97992848b197cfe81960
Theo Kanning <theokanning@gmail.com>
2015-11-01 13:54:28 -0600

    Added BluetoothScanner
    Replacing BluetoothService with Scanner and Connection

--- Android/Dexter/app/src/main/java/theo/dexter/ui/fragment/BluetoothScanningFragment.java
+++ Android/Dexter/app/src/main/java/theo/dexter/ui/fragment/BluetoothScanningFragment.java
@@ -3,11 +3,8 @@ package theo.dexter.ui.fragment;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,15 +17,16 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import theo.dexter.R;
 import theo.dexter.DexterApplication;
+import theo.dexter.R;
+import theo.dexter.bluetooth.BluetoothScanner;
 import theo.dexter.bluetooth.BluetoothService;
 import theo.dexter.ui.activity.PlayActivity;
 
 /**
  * Fragment to choose from paired bluetooth devices
  */
-public class BluetoothFragment extends BaseFragment implements BluetoothService.OnBluetoothDeviceDiscoveredListener, AdapterView.OnItemClickListener{
+public class BluetoothScanningFragment extends BaseFragment implements BluetoothService.OnBluetoothDeviceDiscoveredListener, AdapterView.OnItemClickListener {
 
     private static final int REQUEST_ENABLE_BT = 1;
 
@@ -40,6 +38,8 @@ public class BluetoothFragment extends BaseFragment implements BluetoothService.
 
     private ArrayAdapter<String> adapter;
     private ArrayList<BluetoothDevice> devices;
+
+    private BluetoothScanner bluetoothScanner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,36 +55,31 @@ public class BluetoothFragment extends BaseFragment implements BluetoothService.
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
+        bluetoothScanner = new BluetoothScanner(getContext());
+
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        BluetoothService.bindToService(getActivity(), btConnection);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if(btBound){
-            btService.stopScan();
-            getActivity().unbindService(btConnection);
-        }
+        scan();
     }
 
     /**
      * Clears all data and begins new scan
      */
-    private void scan(){
-        btService.stopScan();
+    private void scan() {
+        //btService.stopScan();
 
         devices = new ArrayList<>();
         adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, new ArrayList<String>());
         deviceList.setAdapter(adapter);
         deviceList.setOnItemClickListener(this);
 
-        btService.startScan(this);
+        bluetoothScanner.startScan(this);
+
+        //btService.startScan(this);
     }
 
 
@@ -100,36 +95,15 @@ public class BluetoothFragment extends BaseFragment implements BluetoothService.
 
         Toast.makeText(getContext(), selected.getAddress(), Toast.LENGTH_SHORT).show();
 
-        if(btService.connect(selected.getAddress())){
+        if (btService.connect(selected.getAddress())) {
             Intent i = new Intent(getActivity(), PlayActivity.class);
             i.putExtra(PlayActivity.EXTRA_ADDRESS, selected.getAddress());
             startActivity(i);
         }
-
-
     }
 
     @Override
     public int getTitleResourceId() {
         return R.string.fragment_bluetooth_title;
     }
-
-    /** Defines callbacks for service binding, passed to bindService() */
-    private ServiceConnection btConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            BluetoothService.BluetoothServiceBinder binder = (BluetoothService.BluetoothServiceBinder) service;
-            btService = binder.getService();
-            btBound = true;
-            scan();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            btBound = false;
-        }
-    };
 }
