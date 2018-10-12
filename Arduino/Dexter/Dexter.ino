f4c04f095fe05e98e25a708a942d6d1fdbf1c8d9
Theo Kanning <theokanning@gmail.com>
2018-10-11 20:32:25 -0500

    Replace MotorShield with A4988
    MotorShield couldn't drive two motors quickly enough, so I replaced it with the A4988 and interrupts on Timer1 and Timer3 of the Teensy.

--- Arduino/Dexter/Dexter.ino
+++ Arduino/Dexter/Dexter.ino
@@ -1,13 +1,7 @@
 #include <SoftwareSerial.h>
-#include <Adafruit_MotorShield.h>
 #include <SerialCommand.h>
-#include <AccelStepper.h>
 #include "Controller.h"
-
-#define LEFT_MOTOR_NUMBER  2
-#define RIGHT_MOTOR_NUMBER 1
-#define STEPS_PER_REVOLUTION 200
-#define REVOLUTIONS_PER_MINUTE 120
+#include <PID_v1.h>
 
 #define BLUETOOTH_TIMEOUT 1000
 #define FREQUENCY 20 // number of motor updates per second
@@ -17,19 +11,12 @@ SoftwareSerial imuSerial(5, 4);
 
 SerialCommand serialCommand(imuSerial);
 
-Adafruit_MotorShield motorManager;
-Adafruit_StepperMotor *motorLeft = motorManager.getStepper(STEPS_PER_REVOLUTION, LEFT_MOTOR_NUMBER);
-Adafruit_StepperMotor *motorRight = motorManager.getStepper(STEPS_PER_REVOLUTION, RIGHT_MOTOR_NUMBER);
-AccelStepper stepperLeft(leftForward, leftBackward);
-AccelStepper stepperRight(rightForward, rightBackward);
-
 Controller controller;
 
 long lastUpdateTimeMs = millis();
 int fallThreshold = 15; // give up if robot is more than this many degrees from vertical
 boolean fallen = false;
 float pitchCorrection = -0.87; // The pitch value when vertical, corrects construction errors
-int count = 0;
 
 void setup() {
   Serial.begin(57600);
@@ -41,28 +28,26 @@ void setup() {
   serialCommand.addCommand("P", pitchReceived);
   serialCommand.addCommand("Y", yawReceived);
   serialCommand.addDefaultHandler(unrecognized);
-  initMotors();
+  enableSteppers();
 }
 
 void loop() {
   checkForFall(controller.pitch);
   
   if (fallen) {
-    //releaseMotors();
-  } else {
-    stepperLeft.runSpeed();
-    stepperRight.runSpeed();
-  }
-  count++;
+    releaseMotors();
+  } 
+  
   if (millis() - lastUpdateTimeMs < 1000 / FREQUENCY) {
     return;
   }
   lastUpdateTimeMs = millis();
-  Serial.println(count);
-  count = 0;
+  
   serialCommand.readSerial();
+  
   MotorSpeed speeds = controller.calculateMotorSpeeds(0);
-  setMotorSpeeds(speeds.left, speeds.right);
+  setLeftSpeed(speeds.left);
+  setRightSpeed(speeds.right);
 }
 
 void rollReceived() {
@@ -93,34 +78,9 @@ void unrecognized() {
   }
 }
 
-void initMotors() {
-  motorManager.begin();
-  stepperLeft.setMaxSpeed(2 * STEPS_PER_REVOLUTION);
-  stepperRight.setMaxSpeed(2 * STEPS_PER_REVOLUTION);
-  stepperLeft.setAcceleration(0); // ignored if set to zero
-  stepperRight.setAcceleration(0);
-}
-
-/*
-* Sets all motors speeds, assumes good data
-*/
-void setMotorSpeeds(float leftMotorSpeed, float rightMotorSpeed) {
-  // setting speed to 0 causes AccelStepper to ignore value and travel at max speed instead.
-  if (leftMotorSpeed == 0) leftMotorSpeed = 0.001;
-  if (rightMotorSpeed == 0) rightMotorSpeed = 0.001;
-  stepperLeft.setSpeed(leftMotorSpeed * STEPS_PER_REVOLUTION);
-  stepperRight.setSpeed((rightMotorSpeed * STEPS_PER_REVOLUTION));
-  
-  //Serial.print("Actual: ");
-  //Serial.print(stepperLeft.speed());
-  //Serial.print(" Expected: ");
-  //Serial.print(leftMotorSpeed * STEPS_PER_REVOLUTION);
-  //Serial.println(" steps/sec");
-}
-
 void releaseMotors(){
-  motorLeft->release();
-  motorRight->release();
+  setLeftSpeed(0);
+  setRightSpeed(0);
 }
 
 void checkForFall(double pitch) {
@@ -142,22 +102,4 @@ void logAngle(char* name, double angle){
   Serial.print(name);
   Serial.print(": ");
   Serial.println(angle);
-}
-
-void leftForward() {
-  motorLeft->onestep(FORWARD, DOUBLE);
-}
-
-void leftBackward() {
-  motorLeft->onestep(BACKWARD, DOUBLE);
-}
-
-void rightForward() {
-  // opposite direction because of motor mounting
-  //motorRight->onestep(BACKWARD, DOUBLE);
-}
-
-void rightBackward() {
-  // opposite direction because of motor mounting
-  //motorRight->onestep(FORWARD, DOUBLE);
 }
---
+++ Arduino/Dexter/Motors.ino
@@ -0,0 +1,50 @@
+#include <TimerOne.h>
+#include <TimerThree.h>
+
+const int maxSpeed = 400; // steps/sec
+
+const int leftStepPin  = 15; // B6
+const int leftDirPin   = 14; // B5
+const int rightStepPin = 13; // B4
+const int rightDirPin  = 12; // D7
+
+void leftStep() {
+  digitalWrite(leftStepPin, HIGH);
+  digitalWrite(leftStepPin, LOW);
+}
+
+void rightStep() {
+  digitalWrite(rightStepPin, HIGH);
+  digitalWrite(rightStepPin, LOW);
+}
+
+void enableSteppers() {
+  pinMode(rightStepPin, OUTPUT);
+  pinMode(rightDirPin, OUTPUT);
+  pinMode(leftStepPin, OUTPUT);
+  pinMode(leftDirPin, OUTPUT);
+  Timer1.initialize(100000);
+  Timer1.attachInterrupt(leftStep);
+  Timer3.initialize(100000);
+  Timer3.attachInterrupt(rightStep);
+}
+
+void setLeftSpeed(int stepsPerSecond) {
+  stepsPerSecond = min(max(stepsPerSecond, -maxSpeed), maxSpeed);
+  Timer1.setPeriod(1000000/abs(stepsPerSecond));
+  setDirection(leftDirPin, stepsPerSecond);
+}
+
+void setRightSpeed(int stepsPerSecond) {
+  stepsPerSecond = min(max(stepsPerSecond, -maxSpeed), maxSpeed);
+  Timer3.setPeriod(1000000/abs(stepsPerSecond));
+  setDirection(rightDirPin, stepsPerSecond);
+}
+
+void setDirection(int pin, int stepsPerSecond) {
+  if (stepsPerSecond > 0) {
+    digitalWrite(pin, HIGH);
+  } else {
+    digitalWrite(pin, LOW);
+  }
+}
